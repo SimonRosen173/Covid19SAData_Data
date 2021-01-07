c8bcda046a119a97d5051c3be818c23286a7fb0a
Simon Rosen <simonrosen173@gmail.com>
2021-01-07 17:12:11 +0200

    Bug fix

--- update.py
+++ update.py
@@ -2,8 +2,9 @@ import sa_data
 import sys
 
 if __name__ == '__main__':
+    print(sys.argv)
     sa_data.preprocess_all()
-    if sys.argv and sys.argv[0] == "pythonanywhere":
+    if len(sys.argv) > 1 and sys.argv[1] == "pythonanywhere":
         pass
     else:
         sa_data.copy_data_local()
